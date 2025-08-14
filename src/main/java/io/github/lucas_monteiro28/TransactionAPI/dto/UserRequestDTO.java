d9bbef4443ba88899c867ecd296156cd10ea7d3a
Lucas Monteiro de Oliveira <lucas.monteiro280898@gmail.com>
2025-08-14 16:43:44 -0300

    feat: UserRequestDTO created

---
+++ src/main/java/io/github/lucas_monteiro28/TransactionAPI/dto/UserRequestDTO.java
@@ -0,0 +1,26 @@
+package io.github.lucas_monteiro28.TransactionAPI.dto;
+
+import io.github.lucas_monteiro28.TransactionAPI.model.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+
+public record UserRequestDTO(
+
+        @NotBlank(message = "Name cannot be blank")
+        String name,
+
+        @NotBlank(message = "Email cannot be blank")
+        @Email(message = "Invalid email")
+        String email,
+
+        @NotBlank(message = "Password cannot be blank")
+        @Length(min = 8, message = "Password must be at least 8 characters long")
+        String password,
+
+        @NotBlank(message = "Role cannot be blank")
+        Role role
+
+) {
+
+}
