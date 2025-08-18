82bfc37dbe94f13be9664e853504680b23d44395
Lucas Monteiro de Oliveira <lucas.monteiro280898@gmail.com>
2025-08-18 13:41:06 -0300

    Updated UserRepository

--- src/main/java/io/github/lucas_monteiro28/TransactionAPI/repositories/UserRepository.java
+++ src/main/java/io/github/lucas_monteiro28/TransactionAPI/repositories/UserRepository.java
@@ -3,8 +3,5 @@ package io.github.lucas_monteiro28.TransactionAPI.repositories;
 import io.github.lucas_monteiro28.TransactionAPI.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findById(Long id);
 }
